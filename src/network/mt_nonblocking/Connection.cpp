c0f3c7fef55b7a2ac84f0df37999f65a1d3d164f
Lena <lena19592@mail.ru>
2020-06-26 23:36:34 +0300

    Homework 5

--- src/network/mt_nonblocking/Connection.cpp
+++ src/network/mt_nonblocking/Connection.cpp
@@ -1,25 +1,183 @@
 #include "Connection.h"
 
+#include <errno.h>
 #include <iostream>
-
+#include <sys/socket.h>
+#include <sys/uio.h>
 namespace Afina {
 namespace Network {
 namespace MTnonblock {
 
 // See Connection.h
-void Connection::Start() { std::cout << "Start" << std::endl; }
+void Connection::Start() {
+
+    _logger->debug("Start connection on {} socket", _socket);
+    _event.data.fd = _socket;
+    _event.data.ptr = this;
+    _event.events = EPOLLIN | EPOLLHUP | EPOLLERR;
+}
 
 // See Connection.h
-void Connection::OnError() { std::cout << "OnError" << std::endl; }
+void Connection::OnError() {
+    _logger->debug("Error on {} socket", _socket);
+    is_alive.store(false, std::memory_order_relaxed);
+}
 
 // See Connection.h
-void Connection::OnClose() { std::cout << "OnClose" << std::endl; }
+void Connection::OnClose() {
+    _logger->debug("Connection on {} socket closed", _socket);
+    is_alive.store(false, std::memory_order_relaxed);
+}
 
 // See Connection.h
-void Connection::DoRead() { std::cout << "DoRead" << std::endl; }
+void Connection::DoRead() {
+    std::atomic_thread_fence(std::memory_order_acquire);
+    try {
+        int cur_readed_bytes = -1;
+        while ((cur_readed_bytes = read(_socket, client_buffer, sizeof(client_buffer))) > 0) {
+            _logger->debug("Got {} bytes from socket", readed_bytes);
+            readed_bytes += cur_readed_bytes;
+            // Single block of data readed from the socket could trigger inside actions a multiple times,
+            // for example:
+            // - read#0: [<command1 start>]
+            // - read#1: [<command1 end> <argument> <command2> <argument for command 2> <command3> ... ]
+            while (readed_bytes > 0) {
+                _logger->debug("Process {} bytes", readed_bytes);
+                // There is no command yet
+                if (!command_to_execute) {
+                    std::size_t parsed = 0;
+                    if (parser.Parse(client_buffer, readed_bytes, parsed)) {
+                        // There is no command to be launched, continue to parse input stream
+                        // Here we are, current chunk finished some command, process it
+                        _logger->debug("Found new command: {} in {} bytes", parser.Name(), parsed);
+                        command_to_execute = parser.Build(arg_remains);
+                        if (arg_remains > 0) {
+                            arg_remains += 2;
+                        }
+                    }
+
+                    // Parsed might fail to consume any bytes from input stream. In real life that could happen,
+                    // for example, because we are working with UTF-16 chars and only 1 byte left in stream
+                    if (parsed == 0) {
+                        break;
+                    } else {
+                        std::memmove(client_buffer, client_buffer + parsed, readed_bytes - parsed);
+                        readed_bytes -= parsed;
+                    }
+                }
+
+                // There is command, but we still wait for argument to arrive...
+                if (command_to_execute && arg_remains > 0) {
+                    _logger->debug("Fill argument: {} bytes of {}", readed_bytes, arg_remains);
+                    // There is some parsed command, and now we are reading argument
+                    std::size_t to_read = std::min(arg_remains, std::size_t(readed_bytes));
+                    argument_for_command.append(client_buffer, to_read);
+
+                    std::memmove(client_buffer, client_buffer + to_read, readed_bytes - to_read);
+                    arg_remains -= to_read;
+                    readed_bytes -= to_read;
+                }
+
+                // There is command & argument - RUN!
+                if (command_to_execute && arg_remains == 0) {
+                    _logger->debug("Start command execution");
+
+                    std::string result;
+                    if (argument_for_command.size()) {
+                        argument_for_command.resize(argument_for_command.size() - 2);
+                    }
+                    command_to_execute->Execute(*pStorage, argument_for_command, result);
+
+                    // Send response
+                    result += "\r\n";
+
+                    output_vec.push_back(result);
+                    if (!(_event.events & EPOLLOUT)) {
+                        _event.events |= EPOLLOUT;
+                    }
+                    if (send(_socket, result.data(), result.size(), 0) <= 0) {
+                        throw std::runtime_error("Failed to send response");
+                    }
+
+                    // Prepare for the next command
+                    command_to_execute.reset();
+                    argument_for_command.resize(0);
+                    parser.Reset();
+                }
+            } // while (readed_bytes)
+        }
+
+        if (readed_bytes == 0) {
+            _logger->debug("Connection closed");
+            data_ready.store(true, std::memory_order_relaxed);
+            std::atomic_thread_fence(std::memory_order_release);
+        } else {
+
+            throw std::runtime_error(std::string(strerror(errno)));
+        }
+    } catch (std::runtime_error &ex) {
+        _logger->error("Failed to process connection on descriptor {}: {}", _socket, ex.what());
+        OnError();
+        data_ready.store(true, std::memory_order_relaxed);
+        std::atomic_thread_fence(std::memory_order_release);
+    }
+}
 
 // See Connection.h
-void Connection::DoWrite() { std::cout << "DoWrite" << std::endl; }
+void Connection::DoWrite() {
+    _logger->debug("DoWrite on {}", _socket);
+    std::atomic_thread_fence(std::memory_order_acquire);
+
+    if (!data_ready.load(std::memory_order_relaxed) || output_vec.empty()) {
+        return;
+    }
+
+    /* struct iovec {
+                void  *iov_base;    // Starting address
+                size_t iov_len;     // Number of bytes to transfer
+            };
+    */
+
+    struct iovec iov[output_vec.size()];
+    iov[0].iov_len = output_vec[0].size() - shift;
+    iov[0].iov_base = &(output_vec[0][0]) + shift;
+
+    for (int i = 1; i < output_vec.size(); i++) {
+        iov[i].iov_len = output_vec[i].size();
+        iov[i].iov_base = &(output_vec[i][0]);
+    }
+
+    int written_bytes = writev(_socket, iov, output_vec.size());
+
+    if (written_bytes <= 0) {
+        // EAGAIN          Resource temporarily unavailable
+        // EINTR           Interrupted function call
+        if (errno == EAGAIN || errno == EINTR) {
+            return;
+        } else {
+            OnError();
+        }
+    }
+
+    int i = 0;
+    for (i = 0; i < output_vec.size(); i++) {
+        if (written_bytes - iov[i].iov_len >= 0) {
+            written_bytes -= iov[i].iov_len;
+        } else {
+            break;
+        }
+    }
+
+    shift = written_bytes;
+    output_vec.erase(output_vec.begin(), output_vec.begin() + i);
+
+    if (output_vec.empty()) {
+        _event.events = EPOLLIN | EPOLLRDHUP | EPOLLERR;
+        is_alive.store(false, std::memory_order_relaxed);
+    }
+
+    std::atomic_thread_fence(std::memory_order_release);
+}
 
 } // namespace MTnonblock
 } // namespace Network
--- src/network/mt_nonblocking/Connection.h
+++ src/network/mt_nonblocking/Connection.h
@@ -1,9 +1,13 @@
 #ifndef AFINA_NETWORK_MT_NONBLOCKING_CONNECTION_H
 #define AFINA_NETWORK_MT_NONBLOCKING_CONNECTION_H
 
+#include <afina/execute/Command.h>
+#include <atomic>
 #include <cstring>
-
+#include <protocol/Parser.h>
+#include <spdlog/logger.h>
 #include <sys/epoll.h>
+#include <vector>
 
 namespace Afina {
 namespace Network {
@@ -11,12 +15,20 @@ namespace MTnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<Afina::Storage> &ps, std::shared_ptr<spdlog::logger> &pl)
+        : _socket(s), pStorage(ps), _logger(pl) {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
+        is_alive.store(true, std::memory_order_release);
+        data_ready.store(false, std::memory_order_release);
+        arg_remains = 0;
+        readed_bytes = 0;
+        shift = 0;
     }
 
-    inline bool isAlive() const { return true; }
+    ~Connection() { close(_socket); }
+
+    inline bool isAlive() const { return is_alive.load(std::memory_order_acquire); }
 
     void Start();
 
@@ -32,6 +44,29 @@ private:
 
     int _socket;
     struct epoll_event _event;
+
+    std::atomic<bool> is_alive;
+    std::atomic<bool> data_ready;
+
+    std::shared_ptr<spdlog::logger> _logger;
+    std::shared_ptr<Afina::Storage> pStorage;
+
+    // Here is connection state
+    // - parser: parse state of the stream
+    // - command_to_execute: last command parsed out of stream
+    // - arg_remains: how many bytes to read from stream to get command argument
+    // - argument_for_command: buffer stores argument
+    std::size_t arg_remains;
+    Protocol::Parser parser;
+    std::string argument_for_command;
+    std::unique_ptr<Execute::Command> command_to_execute;
+
+    int readed_bytes;
+    char client_buffer[4096];
+
+    std::vector<std::string> output_vec;
+
+    int shift;
 };
 
 } // namespace MTnonblock
